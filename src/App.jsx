80a98d2ce9889071a40149de8056b86d9957b1d2
Munish <gupta.munish2k@gmail.com>
2025-03-30 16:19:58 +0530

    first Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import react, { useState } from 'react'
+// import react, { useState } from 'react'
 import Container from "react-bootstrap/Container";
-import TodoList from './Components/TodoList';
-import Objects from './Components/Objects';
+// import TodoList from './Components/TodoList';
+// import Objects from './Components/Objects';
 import TodoMultiple from './Components/TodoMultiple';
-import TodoMultipleCopy from './Components/TodoMultiple';
-import Gadgets from '../../Reducer/src/Components/Gadgets';
+// import TodoMultipleCopy from './Components/TodoMultiple';
+// import Gadgets from '../../Reducer/src/Components/Gadgets';
 
 const App = () => {
   return (
